7019d863f685a22bf88157d39a69abf2cd8d4abf
Godstime <lewisagho47@gmail.com>
2019-08-27 15:56:20 +0100

    Modified Home.js file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-array-index-key */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   API_URL,
   API_KEY,
@@ -16,56 +16,10 @@ import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
 import Spinner from '../elements/Spinner/Spinner';
 import './Home.css';
 
+import { useFetchMovies } from './customHook';
+
 const Home = () => {
-  const [state, setState] = useState({ movies: [] });
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const fetchMovies = async (endpoint) => {
-    setIsError(false);
-    setIsLoading(true);
-
-    // Use URLSearchParams to get URL. Very handy
-    const params = new URLSearchParams(endpoint);
-    if (!params.get('page')) {
-      setState((prev) => ({
-        ...prev,
-        movies: [],
-        searchTerm: params.get('query'),
-      }));
-    }
-
-    try {
-      const result = await (await fetch(endpoint)).json();
-      setState((prev) => ({
-        ...prev,
-        movies: [...prev.movies, ...result.results],
-        heroImage: prev.heroImage || result.results[0],
-        currentPage: result.page,
-        totalPages: result.total_pages,
-      }));
-    } catch (error) {
-      setIsError(true);
-    }
-    setIsLoading(false);
-  };
-
-  // Run once on mount
-  useEffect(() => {
-    if(sessionStorage.getItem('HomeState')) {
-      const persistedState = JSON.parse(sessionStorage.getItem('HomeState'));
-      setState({ ...persistedState });
-    } else {
-      fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
-    }  
-  }, []);
-
-  // Remember state for the next mount if wwe are not in search view
-  useEffect(() => {
-    if(!state.searchTerm) {
-      sessionStorage.setItem('HomeState', JSON.stringify(state));
-    }
-  }, [state])
+  const [{ state, isLoading }, fetchMovies] = useFetchMovies(); 
 
   const searchItems = (searchTerm) => {
     let endpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}`;
